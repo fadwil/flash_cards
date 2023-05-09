9798f33c838c0f04bfee7ee4e8f29647a10ecc0a
Wil Fady <wrfady@gmail.com>
2023-05-09 10:10:49 -0400

    Fix card

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,31 +2,31 @@ require './lib/turn'
 
 RSpec.describe Turn do
     it 'exists' do
-        turn = Turn.new("Juneau", card)
+        turn = Turn.new("Juneau", :card)
 
         expect(turn).to be_instance_of(Turn)
     end
 
     it 'has a guess' do
-        turn = Turn.new("Juneau", card)
+        turn = Turn.new("Juneau", :card)
 
         expect(turn.guess).to eq("Juneau")
     end
 
     it 'has a card' do
-        turn = Turn.new("Juneau", card)
+        turn = Turn.new("Juneau", :card)
 
-        expect(turn.card).to eq(card)
+        expect(turn.card).to eq(:card)
     end
 
     it 'is correct?' do
-        turn = Turn.new("Juneau", card)
+        turn = Turn.new("Juneau", :card)
 
-        expect(turn.guess).to eq true
+        expect(turn.correct?).to eq true
     end
 
     it 'gives feedback' do
-        turn = Turn.new("Juneau", card)
+        turn = Turn.new("Juneau", :card)
 
         expects(turn.feedback).to eq("Correct!")
     end
