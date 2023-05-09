8db8af490963756a06ea46faa07860e68cd2fb7c
Wil Fady <wrfady@gmail.com>
2023-05-09 19:31:31 -0400

    Add card array

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,39 +1,51 @@
 require './lib/deck'
 require './lib/card'
-require 'pry'
+
 
 RSpec.describe Deck do
     it 'exists' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography) 
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        deck = Deck.new(cards)
+        
+        cards = [card_1, card_2, card_3]
 
-        expect(deck).to be_an_instance_of(Turn)
+        deck = Deck.new(cards)
+        
+        expect(deck).to be_instance_of(Deck)
     end
 
-    xit 'has cards' do
+    it 'has cards' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography) 
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        
+        cards = [card_1, card_2, card_3]
+
         deck = Deck.new(cards)
 
         expect(deck.cards).to eq(cards)
     end
     
-    xit 'counts cards' do
+    it 'counts cards' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography) 
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        
+        cards = [card_1, card_2, card_3]
+
         deck = Deck.new(cards)
 
-        expect(deck.count).to eq(cards)
+        expect(deck.count).to eq(cards.length)
     end
 
     xit 'has categories' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography) 
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        
+        cards = [card_1, card_2, card_3]
+
         deck = Deck.new(cards)
 
         expect(deck.cards_in_category).to eq(category)
