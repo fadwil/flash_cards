8e6a229937f8bb193ba14f6a43ff468570005fa1
Wil Fady <wrfady@gmail.com>
2023-05-11 15:03:11 -0400

    Number correct

--- lib/round.rb
+++ lib/round.rb
@@ -16,6 +16,10 @@ class Round
     deck.cards.rotate
     turn
   end
+
+  def number_correct
+    turns.length  
+  end
 end
 
 # The take_turn method is the crux of this problem. The take_turn method takes a string representing the guess. It should create a new Turn object with the appropriate guess and Card. It should store this new Turn, as well as return it from the take_turn method. Also, when the take_turn method is called, the Round should move on to the next card in the deck.
